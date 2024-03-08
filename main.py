a5178d40f9013a27ea2c646eeeec58bd153e5f15
dnadml <162539610+dnadml@users.noreply.github.com>
2024-03-07 21:04:25 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import time
 
 # Import classes and functions from predictionnet module
 # Import functions from base_miner module
-from SNP.predict_dn import predict
-from SNP.get_data_dn import prep_data, scale_data
+from predict_dn import predict
+from get_data_dn import prep_data, scale_data
 
 def get_prediction(timestamp: int) -> None:
     ny_timezone = timezone('America/New_York')
