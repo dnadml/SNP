e22e7186d427436f87a42b5c5cde1c772201beba
snowman1212 <83327960+snowman1212@users.noreply.github.com>
2024-04-02 08:47:17 -0600

    Add files via upload

---
+++ predict_prod.py
@@ -0,0 +1,33 @@
+import joblib
+import numpy as np
+
+from SNP.get_data_prod import prep_data
+
+def predict(timestamp):
+    # Load model and scaler
+    model = joblib.load('/snpOracle/SNP/model_gb.pkl')
+    scaler = joblib.load('/snpOracle/SNP/scaler_gb.pkl')
+
+    # Confirm no missing NA
+    data = prep_data(drop_na=False)
+
+    # Ensure datetime
+    latest_row = data.iloc[-1:].copy()
+    
+    # Extract features for scaling and prediction
+    excluded_columns = ['Datetime', 'NextClose']  # Add any other columns you know should be excluded.
+    features_columns = [col for col in latest_row.columns if col not in excluded_columns]
+
+    latest_features = latest_row[features_columns]
+    
+    # Scale features for loading data
+    latest_features_scaled = scaler.transform(latest_features)
+    
+    # Predict
+    prediction = model.predict(latest_features_scaled)[0]
+
+    # Store prediction in a variable
+    prediction = float(round(prediction, 6))
+    
+    # Return the final prediction
+    return prediction
